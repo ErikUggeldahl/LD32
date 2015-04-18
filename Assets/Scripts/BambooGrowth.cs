8245a71738dc1b55745896bcab0b8942232aa6da
Erik Uggeldahl <erikuggeldahl@gmail.com>
2015-04-17 21:50:23 -0400

    First bamboo growth experiment

---
+++ Assets/Scripts/BambooGrowth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BambooGrowth : MonoBehaviour
+{
+	void Start()
+	{
+		StartCoroutine(Grow());
+	}
+
+	void Update()
+	{
+	}
+
+	IEnumerator Grow()
+	{
+		while (transform.localScale.y < 10)
+		{
+			transform.localScale += new Vector3(0f, 1f, 0f) * Time.deltaTime;
+			yield return null;
+		}
+	}
+}

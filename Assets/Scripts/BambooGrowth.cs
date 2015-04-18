4ac4cdb06e11aa746cb5920e6adbfaf4328aaebe
Erik Uggeldahl <erikuggeldahl@gmail.com>
2015-04-17 22:54:30 -0400

    Added bamboo shoot growth

--- Assets/Scripts/BambooGrowth.cs
+++ Assets/Scripts/BambooGrowth.cs
@@ -3,21 +3,49 @@ using System.Collections;
 
 public class BambooGrowth : MonoBehaviour
 {
-	void Start()
-	{
-		StartCoroutine(Grow());
-	}
+	[SerializeField]
+	Transform nextAnchor;
 
-	void Update()
+	const float startYScale = 0.1f;
+	const float growthTimeSec = 0.01f;
+	const float growthRate = 1f / growthTimeSec;
+
+	public int Index { get;  set; }
+
+	public GrowStalk Stalk { get; set; }
+
+	public void StartGrowth()
 	{
+		transform.localScale = new Vector3(1f, startYScale, 1f);
+
+		StartCoroutine(Grow());
 	}
 
 	IEnumerator Grow()
 	{
-		while (transform.localScale.y < 10)
+		while (transform.localScale.y < 1)
 		{
-			transform.localScale += new Vector3(0f, 1f, 0f) * Time.deltaTime;
+			transform.localScale += new Vector3(0f, growthRate, 0f) * Time.deltaTime;
 			yield return null;
 		}
+
+		transform.localScale = new Vector3(1f, 1f, 1f);
+
+		if (Index < Stalk.SectionCount)
+			GrowNewSegment();
+	}
+
+	void GrowNewSegment()
+	{
+		var nextSegment = Instantiate(gameObject, nextAnchor.position, transform.rotation * Stalk.Lean) as GameObject;
+
+		nextSegment.name = "Segment" + (Index + 1);
+		nextSegment.transform.parent = transform;
+
+		var growth = nextSegment.GetComponent<BambooGrowth>();
+		growth.Index = Index + 1;
+		growth.Stalk = Stalk;
+
+		growth.StartGrowth();
 	}
 }
---
+++ Assets/Scripts/GrowStalk.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowStalk : MonoBehaviour
+{
+	const int minSections = 5;
+	const int maxSections = 20;
+
+	int sectionCount;
+	public int SectionCount { get { return sectionCount; } }
+
+	const float maxLean = 0.05f;
+
+	Quaternion lean;
+	public Quaternion Lean { get { return lean; } }
+
+	[SerializeField]
+	GameObject bambooGO;
+
+	void Start()
+	{
+		sectionCount = Random.Range(minSections, maxSections);
+
+		var leanRand = Random.insideUnitCircle * maxLean;
+		lean = Quaternion.LookRotation(Vector3.forward, Vector3.up + new Vector3(leanRand.x, 1f, leanRand.y));
+
+		var bamboo = Instantiate(bambooGO, transform.position, Quaternion.identity) as GameObject;
+
+		bamboo.name = "Segment0";
+		bamboo.transform.parent = transform;
+
+		var growth = bamboo.GetComponent<BambooGrowth>();
+		growth.Stalk = this;
+		growth.StartGrowth();
+	}
+}

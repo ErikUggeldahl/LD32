39b3b16703390001374e35f0fe18263662589874
Erik Uggeldahl <erikuggeldahl@gmail.com>
2015-04-18 01:53:36 -0400

    Added bamboo sprouting from seeds

---
+++ Assets/Scripts/Bamboo/Seed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seed : MonoBehaviour
+{
+	[SerializeField]
+	GameObject shootGO;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
+			return;
+
+		Instantiate(shootGO, collision.contacts[0].point, Quaternion.identity);
+
+		Destroy(gameObject);
+	}
+}

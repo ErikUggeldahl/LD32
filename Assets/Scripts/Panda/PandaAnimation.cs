c9385d9f85b7e31c01f070c815f06607b910a9df
Erik Uggeldahl <erikuggeldahl@gmail.com>
2015-04-19 14:52:05 -0400

    Added panda prefab and animation script

---
+++ Assets/Scripts/Panda/PandaAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PandaAnimation : MonoBehaviour
+{
+	[SerializeField]
+	Animator animator;
+
+	[SerializeField]
+	bool isSitting = false;
+
+	void Start()
+	{
+		if (isSitting)
+			animator.SetBool("Sitting", isSitting);
+		else
+			StartCoroutine(PlayRandomAnimation());
+	}
+
+	IEnumerator PlayRandomAnimation()
+	{
+		while (true)
+		{
+			animator.SetFloat("SmellChance", Random.value);
+			animator.SetFloat("ScratchChance", Random.value);
+
+			yield return new WaitForSeconds(1f + Random.value);
+		}
+	}
+}
